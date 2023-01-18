9daf008ac41a3a81e1fc000aefd39a5129e59050
AleksandraLos <oluchna@hotmail.com>
2023-01-18 16:43:04 +0100

    imported several components to App.js

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import Navbar from '../../components/Navbar';
+import Hero from '../../components/Hero'
 
 const Home = () => {
   
