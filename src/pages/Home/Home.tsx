66624d2eede53534b17591c8b88b798b98f8f357
Jacob Påhlsson <Jacob.pahlsson@hotmail.com>
2023-01-18 16:28:22 +0100

    Fixed merge bug

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import Hero from '../../components/Hero';
+import Navbar from '../../components/Navbar';
 
 const Home = () => {
   
