42d5855b5cb641877e4ca044d9f0d6a905c46ce5
Jacob Påhlsson <Jacob.pahlsson@hotmail.com>
2023-01-19 14:33:41 +0100

    Added new route and new store

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Outlet, Route, Routes } from 'react-router-dom';
 import LoginForm from './features/users/LoginForm';
 import Home from './pages/Home/Home';
+import userStore from './stores/userStore';
+import store from './stores/store';
+import { observer } from 'mobx-react-lite';
 
 
 
 function App() {
+  const {token} = store;
+  const {getUser} = userStore;
+  
+  useEffect(() => {
+    if(token) {
+      getUser().finally(() => store.setAppLoaded());
+    } else {
+      store.setAppLoaded();
+    }
+  }, [store, userStore])
+
+  // if(!store.appLoaded) return <LoadingComponent />
+
   return (
     <div className="App">
       <Outlet />
@@ -15,4 +31,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
---
+++ src/common/ModalContainer.tsx
@@ -0,0 +1,19 @@
+import { observer } from 'mobx-react-lite';
+import React from 'react'
+import { Modal } from 'semantic-ui-react';
+import modalStore from '../stores/modalStore';
+
+
+const ModalContainer = () => {
+    const {modal, closeModal} = modalStore;
+  return (
+    <Modal open={modal.open} onClose={closeModal} size='mini'>
+        <Modal.Content>
+            {modal.body}
+        </Modal.Content>
+    </Modal>
+  )
+}
+
+
+export default observer(ModalContainer);
