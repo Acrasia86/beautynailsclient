23e5af65d3fee4654eed78fbdc21079a49293a6f
Jacob Påhlsson <Jacob.pahlsson@hotmail.com>
2023-02-02 14:58:05 +0100

    Something something darkside

--- src/components/Booking.tsx
+++ src/components/Booking.tsx
@@ -83,7 +83,7 @@ const Booking = () => {
     }
   </Select>
 </FormControl>
-
+<div>{service}</div>
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Grid container spacing={3}>
         <Grid item xs={12} md={6}>
