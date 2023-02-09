1abaa13b8f2292353ae02b53f1a961e740eda03f
AleksandraLos <oluchna@hotmail.com>
2023-02-09 20:02:52 +0100

    added completed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,95 +1,150 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Unstable_Grid2';
+import { styled, Typography } from "@mui/material";
+import { Box, Container } from "@mui/system";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import PhoneIcon from '@mui/icons-material/Phone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
+const Footer = () => {
+  const CustomContainer = styled(Container)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "space-around",
+    gap: theme.spacing(5),
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+      textAlign: "center",
+    },
+  }));
 
+  const IconBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    gap: "1rem",
+    [theme.breakpoints.down("sm")]: {
+      justifyContent: "center",
+    },
+  }));
 
-export default function Footer() {
+  const FooterLink = styled("span")(({ theme }) => ({
+    fontSize: "16px",
+    color: "#7A7A7E",
+    fontWeight: "300",
+    cursor: "pointer",
+    "&:hover": {
+      color: "#000",
+    },
+  }));
+
   return (
-    <Box component="div" sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid xs={12} md={5} lg={4}>
-          <div>Email subscribe section</div>
-        </Grid>
-        <Grid container xs={12} md={7} lg={8} spacing={4}>
-          <Grid xs={6} lg={3}>
-            <div>
-              <Box component="div"
-                id="category-a"
-                sx={{ fontSize: '12px', textTransform: 'uppercase' }}
-              >
-                Category A
-              </Box>
-              <Box component="ul" aria-labelledby="category-a" sx={{ pl: 2 }}>
-                <li>Link 1.1</li>
-                <li>Link 1.2</li>
-                <li>Link 1.3</li>
-              </Box>
-            </div>
-          </Grid>
-          <Grid xs={6} lg={3}>
-            <div>
-              <Box component="div"
-                id="category-b"
-                sx={{ fontSize: '12px', textTransform: 'uppercase' }}
-              >
-                Category B
-              </Box>
-              <Box component="ul" aria-labelledby="category-b" sx={{ pl: 2 }}>
-                <li>Link 2.1</li>
-                <li>Link 2.2</li>
-                <li>Link 2.3</li>
-              </Box>
-            </div>
-          </Grid>
-          <Grid xs={6} lg={3}>
-            <div>
-              <Box component="div"
-                id="category-c"
-                sx={{ fontSize: '12px', textTransform: 'uppercase' }}
-              >
-                Category C
-              </Box>
-              <Box component="ul" aria-labelledby="category-c" sx={{ pl: 2 }}>
-                <li>Link 3.1</li>
-                <li>Link 3.2</li>
-                <li>Link 3.3</li>
-              </Box>
-            </div>
-          </Grid>
-          <Grid xs={6} lg={3}>
-            <div>
-              <Box component="div"
-                id="category-d"
-                sx={{ fontSize: '12px', textTransform: 'uppercase' }}
-              >
-                Category D
-              </Box>
-              <Box component="ul" aria-labelledby="category-d" sx={{ pl: 2 }}>
-                <li>Link 4.1</li>
-                <li>Link 4.2</li>
-                <li>Link 4.3</li>
-              </Box>
-            </div>
-          </Grid>
-        </Grid>
-          <Grid sx={{ order: { xs: 2, sm: 1 } }}>
-            <div>© Copyright</div>
-          </Grid>
-          <Grid container columnSpacing={1} sx={{ order: { xs: 1, sm: 2 } }}>
-            <Grid>
-              <div>Link A</div>
-            </Grid>
-            <Grid>
-              <div>Link B</div>
-            </Grid>
-            <Grid>
-              <div>Link C</div>
-            </Grid>
-          </Grid>
-        </Grid>
+    <Box component="div" sx={{ py: 10 }}>
+      <CustomContainer>
+        <CustomContainer>
+          <Box component="div">
+            <Typography
+              sx={{
+                fontSize: "20px",
+                color: "#1C1C1D",
+                fontWeight: "700",
+                mb: 2,
+              }}
+            >
+              Adress
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: "16px",
+                color: "#7A7A7E",
+                fontWeight: "500",
+                mb: 2,
+              }}
+            >
+             <LocationOnIcon />Järnvägsgatan 29
+              <br/>
+               25224 Helsingborg
+            </Typography>
+          </Box>
 
+          <Box component='div'>
+            <Typography
+              sx={{
+                fontSize: "20px",
+                color: "#1C1C1D",
+                fontWeight: "700",
+                mb: 2,
+              }}
+            >
+              Öppettider
+            </Typography>
+
+            <Typography
+              sx={{
+                fontSize: "16px",
+                color: "#7A7A7E",
+                fontWeight: "500",
+                mb: 2,
+              }}
+            >
+            Mån - Fre 10:00 - 19:00
+            <br/>
+            Lö 10:00 - 18:00
+            <br/>
+            </Typography>
+          </Box>
+ 
+
+          <Box component="div">
+            <Typography
+              sx={{
+                fontSize: "20px",
+                color: "#1C1C1D",
+                fontWeight: "700",
+                mb: 2,
+              }}
+            >
+              Kontakt
+            </Typography>
+
+            <FooterLink> <PhoneIcon/> 0704329119</FooterLink>
+            <br />
+            <FooterLink> <MailOutlineIcon/> beauty@nails.com</FooterLink>
+            <br />
+
+          </Box>
+
+          <Box component="div">
+            <Typography
+              sx={{
+                fontSize: "20px",
+                color: "#1C1C1D",
+                fontWeight: "700",
+                mb: 2,
+              }}
+            >
+             Följ oss
+            </Typography>
+
+            <Typography
+              sx={{
+                fontSize: "16px",
+                color: "#7A7A7E",
+                fontWeight: "500",
+                mb: 2,
+              }}
+            >
+              Vi finns på sociala medier
+            </Typography>
+            <IconBox>
+            <FacebookIcon style={{ cursor: "pointer" }}/>
+              <InstagramIcon style={{ cursor: "pointer" }}/>
+              <PinterestIcon style={{ cursor: "pointer" }}/>
+            </IconBox>
+          </Box>
+        </CustomContainer>
+      </CustomContainer>
     </Box>
   );
-}
+};
+
+export default Footer;
