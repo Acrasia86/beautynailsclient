7a0b14a6b874ddb5a0ec0f309dc7f02642426f2e
Jacob Påhlsson <Jacob.pahlsson@hotmail.com>
2023-02-14 08:27:07 +0100

    Added users

---
+++ src/pages/AdminPage/Birthdays.tsx
@@ -0,0 +1,35 @@
+import { Card, CardContent, Typography } from '@mui/material';
+import { observer } from 'mobx-react-lite'
+import React, { useEffect } from 'react'
+import SavingsIcon from '@mui/icons-material/Savings';
+import userStore from '../../stores/userStore';
+
+const Birthdays = () => {
+
+    const {birthday, birthdays} = userStore;
+
+    
+  useEffect(() => {
+    birthday();
+  }, [birthdays.length])
+
+  return (
+    <Card sx={{ minWidth: 49 + '%', height: 150 }} className='income2'>
+    <CardContent>
+      <div>
+        <SavingsIcon />
+      </div>
+      <Typography gutterBottom variant="h5" component="div">
+       {birthdays.map((bday) => (
+        bday.userName
+       ))}
+      </Typography>
+      <Typography gutterBottom variant="body2" component="div" sx={{ color: '#555a54' }}>
+        Kunder som fyller år
+      </Typography>
+    </CardContent>
+  </Card>
+  )
+}
+
+export default observer(Birthdays);
